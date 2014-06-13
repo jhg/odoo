cd22c8bfbb130f22db4145709eda4df51283c37f
Jesús Hernández Gormaz <jhg.jesus@gmail.com>
2014-06-13 14:07:21 +0100

    [FIX] error of permission denied when use with uWSGI

--- openerp-wsgi.py
+++ openerp-wsgi.py
@@ -26,7 +26,7 @@ conf = openerp.tools.config
 # Path to the OpenERP Addons repository (comma-separated for
 # multiple locations)
 
-conf['addons_path'] = '../../addons/trunk,../../web/trunk/addons'
+conf['addons_path'] = 'addons,openerp/addons'
 
 # Optional database config if not using local socket
 #conf['db_name'] = 'mycompany'
